cee7bad7d950290893064dfbe982984836a374d5
Raul Cisternas <r.cisternas.w@gmail.com>
2017-06-07 02:47:50 -0400

    UPDATE: computer_spec

--- computer_spec.rb
+++ computer_spec.rb
@@ -1,19 +1,50 @@
-require 'minitest/autorun'
 require_relative 'computer'
+require 'minitest/autorun'
 
 describe Computer do
-
   describe "we are going to test the method 'add' when computers is nil" do
     it "must respond the message: Computer is nil" do
       @computer = nil
-      Notebook.new.add(@computer).must_equal 'Computer is nil'
+      notebook = Notebook.new.add(@computer)
+      notebook.must_equal 'Computer is nil'
+      puts notebook.to_s
     end
   end
 
   describe "we are going to test the method set" do
     it "must respond to array with unique values" do
-      @computers = Set.new ['HP','HP','HP','HP','HP','HP']
-      Desktop.new.add(@computers).must_equal ['HP'].to_set
+      @computers = Set.new ['HP', 'HP', 'HP', 'HP', 'HP']
+      desktop = Desktop.new.add(@computers)
+      desktop.must_equal ["HP"]
+      puts desktop.to_s
+    end
+  end
+
+  describe "we are going to test the method hash" do
+    it "must respond to array with various values" do
+      @computers = Hash[1, 'HP', 2, 'Acer', 3, 'Apple', 4, 'HP', 5, 'HP']
+      desktop = Desktop.new.add(@computers)
+      desktop.must_equal ["HP", "Acer", "Apple", "HP", "HP"]
+      puts desktop.to_s
+    end
+  end
+
+  describe "we are going to test the method array" do
+    it "must respond to array with various values" do
+      @computers = Array.new ['HP', 'Acer', 'Apple', 'HP', 'HP']
+      notebook = Notebook.new.add(@computers)
+      notebook.must_equal ["HP", "Acer", "Apple", "HP", "HP"]
+      puts notebook.to_s
+    end
+  end
+
+  describe "we are going to test the method tree_set" do
+    it "must respond to array with unique values" do
+      @computers = SortedSet.new(['HP', 'HP', 'HP', 'HP', 'HP'])
+      notebook = Notebook.new.add(@computers)
+      notebook.must_equal ["HP"]
+      puts notebook.to_s
     end
   end
 end
+
